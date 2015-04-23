09c423ecaf805a89d91b3279fe5ca8810d54703d
DylanMuir <dylan.richard.muir@gmail.com>
2015-04-23 14:45:02 +0200

    Fixed a bug where a single colon reference would be rejected by MappedTensor and TIFFStack.

--- @MappedTensor/MappedTensor.m
+++ @MappedTensor/MappedTensor.m
@@ -1456,11 +1456,20 @@ end
 % for subscript referencing
 function isvalidsubscript(oRefs)
    try
+      % - Test for colon
+      if (iscolon(oRefs))
+         return;
+      end
+      
       if (islogical(oRefs))
+         % - Test for logical indexing
          validateattributes(oRefs, {'logical'}, {'binary'});
+         
       else
+         % - Test for normal indexing
          validateattributes(oRefs, {'single', 'double'}, {'integer', 'real', 'positive'});
       end
+      
    catch
       error('MappedTensor:badsubscript', ...
             '*** MappedTensor: Subscript indices must either be real positive integers or logicals.');
